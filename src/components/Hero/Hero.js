4679e3f3cabd8e237e9eacd76e49c1e63d8b9c71
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-05 20:31:54 -0400

    hero section components and content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        Another Portfolio
+      </SectionTitle>
+
+      <SectionText>
+        The purpose of this project is to take my development skills to the next level and build awesome apps. 
+      </SectionText>
+
+      <Button onClick={() => window.location = 'https://github.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
